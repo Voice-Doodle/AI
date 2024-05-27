43c0b29e65b98f9f1fcc966f1cf9a81ab172ac33
cksghkd123 <59885850+cksghkd123@users.noreply.github.com>
2024-05-27 12:24:02 +0900

    fix: response type

--- AI_repo/src/routers/conversion.py
+++ AI_repo/src/routers/conversion.py
@@ -7,7 +7,6 @@ import logging
 import aiofiles
 import asyncio
 
-
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 
@@ -23,27 +22,27 @@ router = APIRouter(prefix="/conversion", tags=["Conversion"])
 logger = logging.getLogger(__name__)
 
 @router.post("/{selected_voice}/{pitch_change}")
-async def voice_conversion(selected_voice: str, pitch_change:int, audio_file: UploadFile = File(...)) -> File:
+async def voice_conversion(selected_voice: str, pitch_change: int, audio_file: UploadFile = File(...)) -> FileResponse:
     try:
-        start_time = time.time() # 메소드 실행 !
+        start_time = time.time() # 메소드 실행
         if pitch_change < -24 or pitch_change > 24:
             raise HTTPException(status_code=400, detail="2옥타브 (-24 ~ 24) 범위 내에서 입력해 주세요.")     
 
-        print("파일 이름 만들기 시작: ",time.time()-start_time)
+        print("파일 이름 만들기 시작: ", time.time() - start_time)
         file_name = generate_unique_filename(audio_file.filename)
-        print("파일 이름 만들기 끝: ",time.time()-start_time)
+        print("파일 이름 만들기 끝: ", time.time() - start_time)
 
         # 파일을 디스크에 저장
         input_file_path = f"input/{file_name}"
         async with aiofiles.open(input_file_path, "wb") as file_object:
             await file_object.write(await audio_file.read())
             
-        print("input 파일 저장 끝: ",time.time()-start_time)
-        print("변환 시작: ",time.time()-start_time)
+        print("input 파일 저장 끝: ", time.time() - start_time)
+        print("변환 시작: ", time.time() - start_time)
 
         await async_rvc_convert(selected_voice, input_file_path, file_name, pitch_change)
 
-        print("변환 끝: ",time.time()-start_time)
+        print("변환 끝: ", time.time() - start_time)
 
         response = FileResponse(f"output/{file_name}", filename=file_name)
 
