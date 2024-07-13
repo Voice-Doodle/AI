147987cea84634a9c0b25108af7918d8ec1ad0f7
cksghkd123 <cksghkd1234@naver.com>
2024-07-13 23:00:16 +0900

    로깅 추가

--- AI_repo/src/routers/classification.py
+++ AI_repo/src/routers/classification.py
@@ -3,12 +3,20 @@ import whisper
 import os
 from transformers import pipeline
 
+import logging
+import time
+
 router = APIRouter(prefix="/classification", tags=["Classification"])
 zeroshot_classifier = pipeline("zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v1.1-all-33")
+
+logger = logging.getLogger(__name__)
 
 @router.post("")
 async def text_classification(text: str) -> str:
     try:
+        start_time = time.time()
+        logger.info(f"stt 시작")
+
         ## finetunning을 위한 template
         hypothesis_template = "This record is about {}"
 
@@ -18,6 +26,9 @@ async def text_classification(text: str) -> str:
         output = zeroshot_classifier(text, classes_verbalized, hypothesis_template=hypothesis_template, multi_label=False)
 
         result = output['labels'][0]
+
+        end_time = time.time()
+        logger.info(f"stt 끝 {end_time - start_time}초")
         return result
     except Exception as e:
         return str(e)
--- AI_repo/src/routers/stt.py
+++ AI_repo/src/routers/stt.py
@@ -3,12 +3,19 @@ import whisper
 import os
 from ..utils import generate_unique_filename
 
+import logging
+import time
+
 router = APIRouter(prefix="/stt", tags=["STT"])
 model = whisper.load_model("base")
+
+logger = logging.getLogger(__name__)
 
 @router.post("")
 async def create_stt_file(audio_file: UploadFile = File(...)) -> str:
     try:
+        start_time = time.time()
+        logger.info(f"stt 시작")
         file_name = generate_unique_filename(audio_file.filename)
         # 파일을 디스크에 저장
         file_location = f"input/{file_name}"
@@ -19,7 +26,8 @@ async def create_stt_file(audio_file: UploadFile = File(...)) -> str:
         result = model.transcribe(file_location)
         
         os.remove(file_location)
-
+        end_time = time.time()
+        logger.info(f"stt 끝 {end_time - start_time}초")
         return result['text']
     except Exception as e:
         return str(e)
